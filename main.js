4626c811b4d715993dd7d1d7c7841a393b780793
ethan <izawaryosuke0101@gmail.com>
2020-08-26 17:25:19 +0900

    about form

---
+++ main.js
@@ -0,0 +1,11 @@
+// const form_test1 = document.getElementById("form_test1");
+// console.log(form_test1);
+
+const formTest = document.forms;
+console.log(formTest);
+
+const formTest1 = document.forms.form_test1;
+console.log(formTest1)
+
+const formTestYourNameValue = document.forms.form_test1.your_name.value;
+console.log(formTestYourNameValue)
