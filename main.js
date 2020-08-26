db9f9df2f1a22928c7551e42dbd5283761bb93ac
ethan <izawaryosuke0101@gmail.com>
2020-08-26 18:26:08 +0900

    output reatTime context

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-// const form_test1 = document.getElementById("form_test1");
-// console.log(form_test1);
+//getElementByIdで取得する方法
 
-const formTest = document.forms;
+// const formTest1 = document.getElementById('form_test1');
+// console.log(formTest1);
+
+//.formsで取得する方法
+
+const formTest= document.forms;
+
 console.log(formTest);
 
 const formTest1 = document.forms.form_test1;
-console.log(formTest1)
+console.log(formTest1);
+
+const formTestYourName = document.forms.form_test1.your_name;
+console.log(formTestYourName);
 
 const formTestYourNameValue = document.forms.form_test1.your_name.value;
-console.log(formTestYourNameValue)
+console.log(formTestYourNameValue);
+
+// const formTest2 = document.forms.form_test2;
+// console.log(formTest2);
+
+formTestYourName.addEventListener('input', () => {
+    let yourNameValue = document.getElementById('your_name_value');
+    yourNameValue.textContent = formTestYourName.value.length;
+})
